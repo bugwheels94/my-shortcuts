c1fba39d31b68009210fa1539aff53c89d428687
bugwheels94 <bugwheels94@gmail.com>
2025-07-23 18:16:52 +0530

    fix: sem

--- postpublish.sh
+++ postpublish.sh
@@ -10,5 +10,5 @@ mv "mac-out/dmg/my-shortcuts_0.0.0_aarch64.dmg" "tmp-assets/my-shortcuts-${VERSI
 mv "windows-out/msi/my-shortcuts_0.0.0_x64_en-US.msi" "tmp-assets/my-shortcuts-${VERSION}.msi"
 mv "windows-out/nsis/my-shortcuts_0.0.0_x64-setup.exe" "tmp-assets/my-shortcuts-${VERSION}.exe"
 mv "ubuntu-out/deb/my-shortcuts_0.0.0_amd64.deb" "tmp-assets/my-shortcuts-${VERSION}.deb"
-mv "ubuntu-out/deb/my-shortcuts_0.0.0_amd64.rpm" "tmp-assets/my-shortcuts-${VERSION}.rpm"
+mv "ubuntu-out/rpm/my-shortcuts-0.0.0-1.x86_64.rpm" "tmp-assets/my-shortcuts-${VERSION}.rpm"
 mv "ubuntu-out/appimage/my-shortcuts_0.0.0_amd64.AppImage" "tmp-assets/my-shortcuts-${VERSION}.AppImage"
